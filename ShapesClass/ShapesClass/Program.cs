10c7af20b8d3f8dd5c9119aa81ce4359dbf9d3cd
Ronda Bergman <rondabergman@hotmail.com>
2016-10-05 11:32:47 -0400

    Daily commit

---
+++ ShapesClass/ShapesClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            Circle c2 = new Circle(2);
+            double areaOfCircle = c.Area();
+            
+            Rectangle rect = new Rectangle(10, 5);
+            rect.Area();
+
+            SpecialShape ss = new ShapesClass.SpecialShape();
+            double placeToStoreAnwser = ss.Area();
+            double example = 5;
+
+            Console.WriteLine("The area of my special shape is");
+            Console.WriteLine(ss.Area());
+
+            Square square2 = new Square(5);
+            double valueOfAreaCalculation = square2.Area();
+        }
+    }
+}
---
+++ ShapesClass/ShapesClass/Shape.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesClass
+{
+    public abstract class Shape
+    {
+        //Shortcut to create 2 variables of the same type
+        protected double x, y;
+
+        public Shape()
+        {
+
+        }
+
+        public Shape(double length, double width)
+        {
+            this.x = length;
+            this.y = width;
+        }
+
+        public virtual double Area()
+        {
+            return x * y;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(double r) : base(r, 0)
+        {
+
+        }
+
+        public override double Area()
+        {
+            return x * x * Math.PI;
+        }
+
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle(double wid, double len) : base(len, wid)
+        {
+
+        }
+    }
+
+    public class SpecialShape : Shape
+    {
+        public SpecialShape() : base()
+        {
+
+        }
+    }
+
+    public class Square : Shape
+    {
+        public Square(double side) : base(side, 0)
+        {
+
+        }
+
+        public override double Area()
+        {
+            return x * x;
+        }
+    }
+}
